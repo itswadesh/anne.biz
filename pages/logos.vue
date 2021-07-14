fd8762a898acb6daad15a099805ac28917207e88
Pratap <pratapofficial1111@gmail.com>
2021-07-14 18:05:40 +0530

    logos page modified

--- pages/logos.vue
+++ pages/logos.vue
@@ -1,22 +1,73 @@
 <template>
-  <section>
-    <div class="relative">
-      <img src="/img/logos.jpg" alt="" />
-      <div
+  <section class="relative bg-black">
+    <div class="h-24 md:h-32 relative z-10 flex items-center justify-center">
+      <h1
         class="
-          absolute
-          inset-0
-          bg-gradient-radial
-          from-transparent
-          to-black
-          bg-opacity-50
+          text-white text-xl
+          md:text-2xl
+          xl:text-4xl
+          font-medium
+          text-center
+          px-2
         "
       >
-        <h1 class="text-white text-4xl font-medium mt-36 text-center">
-          1 million+ developers trust the first choice for quality apps
-        </h1>
+        1 million+ developers trust the first choice for quality apps
+      </h1>
+    </div>
+
+    <div>
+      <div class="flex justify-center -mx-10">
+        <img
+          v-for="img in 22"
+          :key="img"
+          src="/img/visa.png"
+          alt=""
+          class="h-10 w-10 lg:h-16 lg:w-16 m-1 rounded bg-white object-contain"
+        />
+      </div>
+      <div class="flex justify-center -mx-10">
+        <img
+          v-for="img in 22"
+          :key="img"
+          src="/img/visa.png"
+          alt=""
+          class="h-10 w-10 lg:h-16 lg:w-16 m-1 rounded bg-white object-contain"
+        />
+      </div>
+      <div class="flex justify-center -mx-10">
+        <img
+          v-for="img in 22"
+          :key="img"
+          src="/img/visa.png"
+          alt=""
+          class="h-10 w-10 lg:h-16 lg:w-16 m-1 rounded bg-white object-contain"
+        />
+      </div>
+      <div class="flex justify-center -mx-10">
+        <img
+          v-for="img in 22"
+          :key="img"
+          src="/img/visa.png"
+          alt=""
+          class="h-10 w-10 lg:h-16 lg:w-16 m-1 rounded bg-white object-contain"
+        />
       </div>
     </div>
+
+    <div
+      class="h-24 md:h-32 relative z-10 flex items-center justify-center"
+    ></div>
+
+    <div
+      class="
+        absolute
+        inset-0
+        bg-gradient-radial
+        from-transparent
+        via-transparent
+        to-black
+      "
+    ></div>
   </section>
 </template>
 

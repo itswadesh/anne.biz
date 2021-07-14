59ea1f5d201383bd171599fce3722b0e2e8b5a89
Pratap <pratapofficial1111@gmail.com>
2021-07-14 12:02:35 +0530

    new logo background page added

---
+++ pages/logos.vue
@@ -0,0 +1,18 @@
+<template>
+  <section>
+    <div class="relative">
+      <img src="/img/logos.jpg" alt="" />
+      <div class="absolute inset-0 bg-black bg-opacity-50">
+        <h1 class="text-white text-4xl font-medium mt-24 text-center">
+          1 million+ developers trust the first choice for quality apps
+        </h1>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped></style>

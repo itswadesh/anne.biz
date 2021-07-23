fb62cc99d016727c97ad7899dda5d1958a3b2ec7
Pratap <pratapofficial1111@gmail.com>
2021-07-23 13:48:08 +0530

    login page

--- components/ui/GrnIndGradiantButton.vue
+++ components/ui/GrnIndGradiantButton.vue
@@ -70,15 +70,8 @@ export default {
 <style scoped>
 .gradient {
   display: flex;
-  background: linear-gradient(
-    120deg,
-    #10b981,
-    #6366f1,
-    #10b981,
-    #6366f1,
-    #10b981
-  );
-  background-size: 300%;
+  background: linear-gradient(120deg, #fb7801, #000000, #fb7801);
+  background-size: 200%;
   transition: 0.8s;
 }
 
